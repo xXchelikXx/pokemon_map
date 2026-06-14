0f7798418a84e17eb9d2c7f18fc0e52e8d83f2dc
xXchelikXx <162018732+xXchelikXx@users.noreply.github.com>
2026-06-14 10:55:27 +0300

    изменил CharField на TextFoeld

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -5,7 +5,7 @@ class Pokemon(models.Model):
     title_en = models.CharField(max_length=200, blank=True, verbose_name="имя покемона(англ)")
     title_jp = models.CharField(max_length=200, blank=True, verbose_name="имя покемона(яп)")
     image = models.ImageField(upload_to="pokemon", blank=True, null=True, verbose_name="картинка покемона")
-    description = models.CharField(max_length=500, blank=True, verbose_name="описание")
+    description = models.TextField(max_length=500, blank=True, verbose_name="описание")
     previous_evolution = models.ForeignKey("self", on_delete=models.CASCADE, blank=True, null=True, verbose_name="предыдущая эволюция", related_name="next_evolution")
     
 
